d40510bd7c98b7966c9d57ef7666045ab840c982
Prayag Jain <prayagjain2@gmail.com>
2026-04-16 17:29:06 +0530

    build: perform basic setup

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Hugo development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          hugo
+          go
+          nodejs
+        ];
+
+        shellHook = ''
+          echo "Hugo dev environment ready"
+        '';
+      };
+    };
+}
